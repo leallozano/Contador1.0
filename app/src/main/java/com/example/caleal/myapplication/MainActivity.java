8af5ae5a72756c7c2f90d971caf8444e32c9f766
Carlos <leal.lozano@gmail.com>
2017-05-23 20:03:51 -0400

    Actualización contador

--- app/src/main/java/com/example/caleal/myapplication/MainActivity.java
+++ app/src/main/java/com/example/caleal/myapplication/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,7 +27,24 @@ public class MainActivity extends AppCompatActivity {
         contador=0;
         textoResultado=(TextView)findViewById(R.id.contadorTexto);
 
+        EventoTeclado teclado= new EventoTeclado();
+
+        EditText n_reseteo=(EditText)findViewById(R.id.n_reseteo);
+
+        n_reseteo.setOnEditorActionListener(teclado);
     }
+
+    class EventoTeclado implements TextView.OnEditorActionListener{
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+
+            if (actionId== EditorInfo.IME_ACTION_DONE) {
+                reseteaContador(null);
+            }
+            return false;
+        }
+    }
+
     public void incrementaContador(View vista) {
         contador++;
         textoResultado.setText(""+contador);
@@ -55,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
         numero_reset.setText("");
         textoResultado.setText(""+contador);
+
+        InputMethodManager miTeclado=(InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        miTeclado.hideSoftInputFromWindow(numero_reset.getWindowToken(),0);
 
     }
 
